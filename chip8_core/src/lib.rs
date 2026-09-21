5b7ad90816d3fd6c7dad367beee850c7c020c9bd
Hawre Aziz <hawreaziz698@gmail.com>
2026-09-21 23:58:29 +0200

    Add more opcodes

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -1,4 +1,4 @@
-use core::num;
+use core::{num, panic};
 use std::{fs::File, io::Read};
 
 use log::debug;
@@ -141,6 +141,57 @@ impl Chip8 {
                 );
                 self.pc = self.inst.nnn;
             }
+            (0x2, _, _, _) => {
+                if self.stack_ptr as usize >= self.stack.len() {
+                    panic!("Stack overflow, stack out of space");
+                }
+                self.stack[self.stack_ptr as usize] = self.pc;
+                self.stack_ptr += 1;
+                self.pc = self.inst.nnn;
+            }
+            (0, 0, 0xE, 0xE) => {
+                debug!(
+                    "{info} return from subroutine pc ({:x}) = stack_ptr {:x} = {:02x}",
+                    self.pc,
+                    self.stack_ptr,
+                    (self.stack_ptr - 1)
+                );
+                if self.stack_ptr == 0 {
+                    panic!("Stack overflow, stack reached 0");
+                }
+                self.stack_ptr -= 1;
+                self.pc = self.stack[self.stack_ptr as usize];
+            }
+            (0x3, _, _, _) => {
+                debug!(
+                    "{info} Skip next instruction if V{:x} ({:02x}) == NN ({:02x})",
+                    self.inst.x, self.v_reg[self.inst.x as usize], self.inst.nn
+                );
+                if self.v_reg[self.inst.x as usize] == self.inst.nn {
+                    self.pc += 2;
+                }
+            }
+            (0x4, _, _, _) => {
+                debug!(
+                    "{info} Skip next instruction if V{:x} ({:02x}) != NN ({:02x})",
+                    self.inst.x, self.v_reg[self.inst.x as usize], self.inst.nn
+                );
+                if self.v_reg[self.inst.x as usize] != self.inst.nn {
+                    self.pc += 2;
+                }
+            }
+            (0x5, _, _, 0) => {
+                debug!(
+                    "{info} Skip next instruction if V{:x} ({:02x}) ==  V{:x} ({:02x})",
+                    self.inst.x,
+                    self.v_reg[self.inst.x as usize],
+                    self.inst.y,
+                    self.v_reg[self.inst.y as usize]
+                );
+                if self.v_reg[self.inst.x as usize] == self.v_reg[self.inst.y as usize] {
+                    self.pc += 2;
+                }
+            }
             (0x6, _, _, _) => {
                 debug!(
                     "{info} Set V0x{:02x} (0x{:02x}) to NN (0x{:02x})",
@@ -154,10 +205,139 @@ impl Chip8 {
                     self.inst.x,
                     self.v_reg[self.inst.x as usize],
                     self.inst.nn,
-                    (self.v_reg[self.inst.x as usize] + self.inst.nn)
+                    (self.v_reg[self.inst.x as usize].wrapping_add(self.inst.nn))
                 );
                 self.v_reg[self.inst.x as usize] =
                     self.v_reg[self.inst.x as usize].wrapping_add(self.inst.nn);
+            }
+            (0x8, _, _, 0) => {
+                debug!(
+                    "{info} Set V0x{:x} (0x{:02x}) to V0x{:x} (0x{:x}), result = 0x{:x}",
+                    self.inst.x,
+                    self.v_reg[self.inst.x as usize],
+                    self.inst.y,
+                    self.v_reg[self.inst.y as usize],
+                    self.v_reg[self.inst.y as usize]
+                );
+                self.v_reg[self.inst.x as usize] = self.v_reg[self.inst.y as usize];
+            }
+            (0x8, _, _, 1) => {
+                debug!(
+                    "{info} Set V0x{:x} (0x{:02x}) to V0x{:x} |= V0x{:x} (0x{:x}), result = 0x{:x}",
+                    self.inst.x,
+                    self.v_reg[self.inst.x as usize],
+                    self.inst.x,
+                    self.inst.y,
+                    self.v_reg[self.inst.y as usize],
+                    self.v_reg[self.inst.x as usize] | self.v_reg[self.inst.y as usize]
+                );
+                self.v_reg[self.inst.x as usize] |= self.v_reg[self.inst.y as usize];
+            }
+            (0x8, _, _, 2) => {
+                debug!(
+                    "{info} Set V0x{:x} (0x{:02x}) to V0x{:x} &= V0x{:x} (0x{:x}), result = 0x{:x}",
+                    self.inst.x,
+                    self.v_reg[self.inst.x as usize],
+                    self.inst.x,
+                    self.inst.y,
+                    self.v_reg[self.inst.y as usize],
+                    self.v_reg[self.inst.x as usize] & self.v_reg[self.inst.y as usize]
+                );
+                self.v_reg[self.inst.x as usize] &= self.v_reg[self.inst.y as usize];
+            }
+            (0x8, _, _, 3) => {
+                debug!(
+                    "{info} Set V0x{:x} (0x{:02x}) to V0x{:x} ^= V0x{:x} (0x{:x}), result = 0x{:x}",
+                    self.inst.x,
+                    self.v_reg[self.inst.x as usize],
+                    self.inst.x,
+                    self.inst.y,
+                    self.v_reg[self.inst.y as usize],
+                    self.v_reg[self.inst.x as usize] ^ self.v_reg[self.inst.y as usize]
+                );
+                self.v_reg[self.inst.x as usize] ^= self.v_reg[self.inst.y as usize];
+            }
+            (0x8, _, _, 4) => {
+                let (value, overflow) = self.v_reg[self.inst.x as usize]
+                    .overflowing_add(self.v_reg[self.inst.y as usize]);
+                let rest: u8 = if overflow { 1 } else { 0 };
+
+                debug!(
+                    "{info} Add V0x{:02x} (0x{:02x}) + V0x{:x} (0x{:02x}) = 0x{:02x}, set VF to {:}",
+                    self.inst.x,
+                    self.v_reg[self.inst.x as usize],
+                    self.inst.y,
+                    self.v_reg[self.inst.y as usize],
+                    value,
+                    rest
+                );
+                self.v_reg[self.inst.x as usize] = value;
+                self.v_reg[0xF] = rest;
+            }
+            (0x8, _, _, 5) => {
+                let (value, underflow) = self.v_reg[self.inst.x as usize]
+                    .overflowing_sub(self.v_reg[self.inst.y as usize]);
+                let rest = if underflow { 0 } else { 1 };
+                debug!(
+                    "{info} Add V0x{:02x} (0x{:02x}) - V0x{:x} (0x{:02x}) = 0x{:02x}, set VF to {:}",
+                    self.inst.x,
+                    self.v_reg[self.inst.x as usize],
+                    self.inst.y,
+                    self.v_reg[self.inst.y as usize],
+                    value,
+                    rest
+                );
+                self.v_reg[self.inst.x as usize] = value;
+                self.v_reg[0xF] = rest;
+            }
+            (0x8, _, _, 6) => {
+                debug!(
+                    "{info} V0x{:x} (0x{:x}) >>= 1",
+                    self.inst.x, self.v_reg[self.inst.x as usize]
+                );
+                let least_bit = self.v_reg[self.inst.x as usize] & 1;
+                self.v_reg[self.inst.x as usize] >>= 1;
+                self.v_reg[0xF] = least_bit;
+            }
+            (0x8, _, _, 7) => {
+                let (value, underflow) = self.v_reg[self.inst.y as usize]
+                    .overflowing_sub(self.v_reg[self.inst.x as usize]);
+                let rest = if underflow { 0 } else { 1 };
+                debug!(
+                    "{info} V0x{:x} (0x{:x}) = V0x{:02x} (0x{:02x}) - V0x{:x} (0x{:02x}) = 0x{:02x}, set VF to {:}",
+                    self.inst.y,
+                    self.v_reg[self.inst.y as usize],
+                    self.inst.y,
+                    self.v_reg[self.inst.y as usize],
+                    self.inst.x,
+                    self.v_reg[self.inst.x as usize],
+                    value,
+                    rest
+                );
+                self.v_reg[self.inst.x as usize] = value;
+                self.v_reg[0xF] = rest;
+            }
+            (0x8, _, _, 0xE) => {
+                debug!(
+                    "{info} V0x{:x} (0x{:x}) <<= 1",
+                    self.inst.x, self.v_reg[self.inst.x as usize]
+                );
+
+                let most_sig = (self.v_reg[self.inst.x as usize] >> 7) & 1;
+                self.v_reg[self.inst.x as usize] <<= 1;
+                self.v_reg[0xF] = most_sig;
+            }
+            (0x9, _, _, 0) => {
+                debug!(
+                    "{info} Skip next instruction if V{:x} ({:02x}) !=  V{:x} ({:02x})",
+                    self.inst.x,
+                    self.v_reg[self.inst.x as usize],
+                    self.inst.y,
+                    self.v_reg[self.inst.y as usize]
+                );
+                if self.v_reg[self.inst.x as usize] != self.v_reg[self.inst.y as usize] {
+                    self.pc += 2;
+                }
             }
             (0xA, _, _, _) => {
                 debug!(
@@ -171,10 +351,9 @@ impl Chip8 {
                 // extract x and cordinates
                 let x_coords = self.v_reg[self.inst.x as usize] as usize % SCREEN_WIDTH;
                 let y_coords = self.v_reg[self.inst.y as usize] as usize % SCREEN_HEIGHT;
-                let num_rows: usize = self.inst.n as usize;
 
                 self.v_reg[0xF] = 0;
-                for row in 0..num_rows {
+                for row in 0..self.inst.n as usize {
                     let sprite_byte = self.ram[self.i as usize + row];
                     for col in 0..8 {
                         if (sprite_byte & (0b10000000 >> col)) != 0 {
@@ -188,14 +367,76 @@ impl Chip8 {
                         }
                     }
                 }
-
-                // loop in N rows
-                // extract each row
-                // loop through all the bits
-                // if dispaly at x,y is on and pixel bit is also on turn off and set VF = 1
-                // make sure your handler the edge cases
             }
-            _ => debug!("Unimplemented opcode {:04x}", opcode),
+            (0xF, _, 0x0, 0x7) => {
+                debug!(
+                    "{info} V0x{:x} (0x{:x}) = delay_timer {}",
+                    self.inst.x, self.v_reg[self.inst.x as usize], self.delay_timer
+                );
+                self.v_reg[self.inst.x as usize] = self.delay_timer;
+            }
+            (0xF, _, 0x0, 0xA) => {
+                debug!(
+                    "{info} if Any key pressed block and wait for key input, set VX to value of key"
+                );
+                let mut pressed = false;
+                for i in 00..self.keypad.len() {
+                    if self.keypad[i] {
+                        pressed = true;
+                        self.v_reg[self.inst.x as usize] = i as u8;
+                        break;
+                    }
+                }
+                if !pressed {
+                    self.pc -= 2;
+                }
+            }
+            (0xF, _, 0x1, 0x5) => {
+                debug!(
+                    "{info} Set delay_timer {} to V{:x} (0x{:x})",
+                    self.delay_timer, self.inst.x, self.v_reg[self.inst.x as usize]
+                );
+                self.delay_timer = self.v_reg[self.inst.x as usize];
+            }
+            (0xF, _, 0x1, 0x8) => {
+                debug!(
+                    "{info} Set sound_timer {} to V{:x} (0x{:x})",
+                    self.sound_timer, self.inst.x, self.v_reg[self.inst.x as usize]
+                );
+                self.sound_timer = self.v_reg[self.inst.x as usize];
+            }
+            (0xF, _, 0x1, 0xE) => {
+                debug!(
+                    "{info} I (0x{:x}) += V0x{:x} (0x{:x}) = {:x}",
+                    self.i,
+                    self.inst.x,
+                    self.v_reg[self.inst.x as usize],
+                    self.i.wrapping_add(self.v_reg[self.inst.x as usize] as u16)
+                );
+                self.i = self.i.wrapping_add(self.v_reg[self.inst.x as usize] as u16);
+            }
+
+            (0xF, _, 0x2, 0x9) => {
+                debug!(
+                    "{info} Set I 0x{:x} to sprite location memory for character in V0x{:x} (0x{:x}) = 0x{:04x}",
+                    self.i,
+                    self.inst.x,
+                    self.v_reg[self.inst.x as usize],
+                    (self.v_reg[self.inst.x as usize] * 5)
+                );
+                // each sprite is 5 rows height
+                self.i = self.v_reg[self.inst.x as usize] as u16 * 5;
+            }
+            (0xF, _, 0x3, 0x3) => {
+                debug!("{info}");
+            }
+            (0xF, _, 0x5, 0x5) => {
+                debug!("{info}");
+            }
+            (0xF, _, 0x6, 0x5) => {
+                debug!("{info}");
+            }
+            _ => debug!("Unimplemented opcode 0x{:04x}", opcode),
         }
     }
 }
